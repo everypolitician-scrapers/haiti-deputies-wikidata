c67a35d3404489fceb1154b1b45d3bca03f94f73
Tony Bowden <tony@tmtm.com>
2017-05-22 09:58:46 +0100

    Also scrape Haitian category

--- scraper.rb
+++ scraper.rb
@@ -5,5 +5,6 @@ require 'wikidata/fetcher'
 
 en = WikiData::Category.new('Category:Members_of_the_Chamber_of_Deputies_(Haiti)', 'en').member_titles
 fr = WikiData::Category.new('Catégorie:Personnalité politique haïtienne', 'fr').member_titles
+ht = WikiData::Category.new('Kategori:Politisyen ayisyen', 'ht').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { fr: fr, en: en })
+EveryPolitician::Wikidata.scrape_wikidata(names: { fr: fr, ht: ht, en: en })
